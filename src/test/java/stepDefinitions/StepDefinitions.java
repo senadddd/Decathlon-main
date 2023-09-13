0212cb232c310d5ead5dc84dba04a6cd65d78d7e
aatta <a.atta.gondal@gmail.com>
2023-09-13 11:21:02 +0200

    initial puch

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-
+/*bjhh*/
 public class StepDefinitions {
 	DecaLongJump decaLongJump = new DecaLongJump();
 	int score;
